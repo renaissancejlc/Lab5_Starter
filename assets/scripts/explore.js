99b67c90d8725e8899f48b0a4158ae05f37f8dd8
renaissance.jlc <rjcarr@ucsd.edu>
2021-10-24 20:03:15 -0700

    completed expose

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,54 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var synth = window.speechSynthesis;
+
+var inputForm = document.querySelector('button');
+var inputTxt = document.querySelector('textarea');
+var voiceSelect = document.querySelector('select');
+
+/*var pitch = document.querySelector('#pitch');
+var pitchValue = document.querySelector('.pitch-value');
+var rate = document.querySelector('#rate');
+var rateValue = document.querySelector('.rate-value');*/
+var img = document.querySelector("img");
+var voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+  for (var i = 0 ; i < voices.length ; i++){
+    console.log(voices[i]);
+
+  }
+  for(var i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+inputForm.addEventListener("click", function(event){
+  event.preventDefault();
+  var utter = new SpeechSynthesisUtterance(inputTxt.value);
+  var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for(var i = 0; i < voices.length ; i++) {
+    if(voices[i].name === selectedOption) {
+      utter.voice = voices[i];
+    }
+  }
+  synth.speak(utter);
+  inputTxt.blur();
+});
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,60 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // TODO  
+  let img = document.querySelectorAll('img');
+  //are the if statements for the image declarations? shouldn't they be static
+  let el = document.getElementById("horn-select");
+  el.addEventListener("change", function(){
+    if (el.value == "air-horn"){
+      img[0].src = "assets/images/air-horn.svg";
+    }else if (el.value == "car-horn"){
+      img[0].src = "assets/images/car-horn.svg";
+    }else if (el.value == "party-horn"){
+      img[0].src = "assets/images/party-horn.svg";
+    }else{
+      img[0].src = "assets/images/no-image.png"
+    }
+  });
+  let audio = document.querySelector("audio")
+  let vol = document.getElementById("volume");
+  vol.addEventListener("input", function(){
+    if (vol.value == 0){
+      img[1].src = "assets/icons/volume-level-0.svg";
+    }else if (vol.value >= 1 && vol.value < 33){
+      img[1].src = "assets/icons/volume-level-1.svg";
+    }else if (vol.value >= 33 && vol.value < 67){
+      img[1].src = "assets/icons/volume-level-2.svg";
+    }else{ //vol.val >=67
+      img[1].src = "assets/icons/volume-level-3.svg" //not needed
+    }
+    audio.volume = vol.value / 100;
+  });
+  let audioButt = document.querySelector("button");
+  audioButt.addEventListener("click", function(){
+    if (el.value == "air-horn"){
+      audio.src = "assets/audio/air-horn.mp3";
+      audio.play();
+    }else if (el.value == "car-horn"){
+      audio.src = "assets/audio/car-horn.mp3";
+      audio.play();
+    }else if (el.value == "party-horn"){
+      audio.src = "assets/audio/party-horn.mp3";
+      audio.play();
+      let confetti = new JSConfetti();
+      confetti.addConfetti({ confettiNumber:3001,
+        emojis: ['🌈', '💥', '✨', '🔥', '🌸'],
+
+      })
+    }
+  });
+/*
+At zero volume, the mute icon (level 0) should be displayed
+From 1 to < 33 volume the first volume level should be displayed
+From 33 to < 67 volume the second volume level should be displayed
+From 67 and up the third volume level should be displayed
+The correct volume icon should be set
+The corresponding volume should be set for the audio element (note: this element’s volume is not out of 100)
+*/
+
 }
